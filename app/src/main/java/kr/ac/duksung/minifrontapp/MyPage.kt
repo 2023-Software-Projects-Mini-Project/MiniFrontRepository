4dd3253921268208cd3273858b3998a38aa3e9ac
dilma01 <dilma01@naver.com>
2023-09-29 06:30:27 +0900

    오늘의 메뉴 페이지, 마이페이지 추가

---
+++ app/src/main/java/kr/ac/duksung/minifrontapp/MyPage.kt
@@ -0,0 +1,11 @@
+package kr.ac.duksung.minifrontapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_page)
+    }
+}
