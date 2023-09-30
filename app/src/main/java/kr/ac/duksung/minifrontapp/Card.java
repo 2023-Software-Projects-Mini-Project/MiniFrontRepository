5c1533afad535c305de3eced02e7b186a179b468
dilma01 <dilma01@naver.com>
2023-10-01 00:44:17 +0900

    카드 결제 관리 페이지 추가

---
+++ app/src/main/java/kr/ac/duksung/minifrontapp/Card.java
@@ -0,0 +1,14 @@
+package kr.ac.duksung.minifrontapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Card extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card);
+    }
+}
