1a6418f9c7bc73c51fae02912fb477ebfcbb9b24
ppopik35@gmail.com <ppopik35@gmail.com>
2023-09-30 17:06:06 +0900

    age_1

---
+++ app/src/main/java/kr/ac/duksung/minifrontapp/MainActivity.kt
@@ -0,0 +1,14 @@
+package kr.ac.duksung.minifrontapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // 이건 테스트입니다.
+        // 2
+    }
+}
