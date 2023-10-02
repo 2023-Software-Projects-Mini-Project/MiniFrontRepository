ff29a37a17c674179153132a0de95df514e594b6
dilma01 <dilma01@naver.com>
2023-10-02 23:52:39 +0900

    친구 목록 페이지 추가

---
+++ app/src/main/java/kr/ac/duksung/minifrontapp/FriendsList.kt
@@ -0,0 +1,11 @@
+package kr.ac.duksung.minifrontapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FriendsList : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_friends_list)
+    }
+}
