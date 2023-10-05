2f326ae16425b4be58a025a0230247dbcfebc048
ppopik35@gmail.com <ppopik35@gmail.com>
2023-10-06 06:45:32 +0900

    age_17 BFoodActivity 네비바

--- app/src/main/java/kr/ac/duksung/minifrontapp/BFoodActivity.kt
+++ app/src/main/java/kr/ac/duksung/minifrontapp/BFoodActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.boonski_list.*
 import kotlinx.android.synthetic.main.boonski_list.back_icon
+import kotlinx.android.synthetic.main.boonski_list.bottomNavigationView
+import kotlinx.android.synthetic.main.cfood_list.*
 
 class BFoodActivity : AppCompatActivity() {
 
@@ -19,6 +21,28 @@ class BFoodActivity : AppCompatActivity() {
                 finish() // 현재 액티비티 종료
             }
         })
+
+        // 바텀 네비게이션 아이템 클릭 리스너 설정
+        bottomNavigationView.setOnNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.page_home -> {
+                    // 홈 아이템 클릭 시 홈 화면으로 이동
+                    startActivity(Intent(this@BFoodActivity, HomeFragment::class.java))
+                    true
+                }
+                R.id.page_fv -> {
+                    // 친구 추가 아이템 클릭 시 친구 추가 화면으로 이동
+                    startActivity(Intent(this@BFoodActivity, FriendsList::class.java))
+                    true
+                }
+                R.id.page_ps -> {
+                    // 마이페이지 아이템 클릭 시 마이페이지 화면으로 이동
+                    startActivity(Intent(this@BFoodActivity, MyPage::class.java))
+                    true
+                }
+                else -> false
+            }
+        }
 
         goKimbab.setOnClickListener{
             val intent = Intent(this@BFoodActivity, MenuReviewActivity::class.java)
