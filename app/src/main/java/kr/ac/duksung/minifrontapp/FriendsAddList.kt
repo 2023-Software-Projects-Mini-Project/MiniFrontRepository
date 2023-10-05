a3c35bce591144ec8122e9b5a4a60554d9f8fdba
dilma01 <dilma01@naver.com>
2023-10-06 00:07:05 +0900

    친구 추가1

---
+++ app/src/main/java/kr/ac/duksung/minifrontapp/FriendsAddList.kt
@@ -0,0 +1,11 @@
+package kr.ac.duksung.minifrontapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FriendsAddList : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_friends_add_list)
+    }
+}
