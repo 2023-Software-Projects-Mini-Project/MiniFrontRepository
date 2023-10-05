495a5dcc4f6b91e299133066c53e759ba24efba1
dilma01 <dilma01@naver.com>
2023-10-05 16:54:03 +0900

    주문내역 Adapter 추가

---
+++ app/src/main/java/kr/ac/duksung/minifrontapp/OrderDetails.kt
@@ -0,0 +1,13 @@
+package kr.ac.duksung.minifrontapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OrderDetails : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_order_details)
+
+
+    }
+}
---
+++ app/src/main/java/kr/ac/duksung/minifrontapp/OrderDetailsClass.kt
@@ -0,0 +1,3 @@
+package kr.ac.duksung.minifrontapp
+
+data class OrderDetailsClass (val id: Int, val category: String, val count: Int, sumprice: Int)
