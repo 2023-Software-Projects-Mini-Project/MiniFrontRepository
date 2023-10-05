8cf1078652115ff56088f32c366183f04f39ecc5
kmin82 <kym01120921@gmail.com>
2023-10-05 22:31:09 +0900

    회원가이-로그인

--- app/src/main/java/kr/ac/duksung/minifrontapp/MenuReviewActivity.kt
+++ app/src/main/java/kr/ac/duksung/minifrontapp/MenuReviewActivity.kt
@@ -17,5 +17,43 @@ class MenuReviewActivity : AppCompatActivity() {
                 finish() // 현재 액티비티 종료
             }
         })
+/*
+        public void onGoKimchiClick(View viewpublic void onGoKimchiClick(View view) {
+            // 리뷰 화면으로 이동하는 인텐트 생성
+            Intent intent = new Intent(this, ReviewActivity.class);
+
+            // 인텐트에 음식 이름 또는 기타 데이터를 추가할 수 있음
+            intent.putExtra("foodName", "김치찌개");
+
+            // 리뷰 화면으로 이동
+            startActivity(intent);
+        }
+        ) {
+            // 리뷰 화면으로 이동하는 인텐트 생성
+            Intent intent = new Intent(this, ReviewActivity.class);
+
+            // 인텐트에 음식 이름 또는 기타 데이터를 추가할 수 있음
+            intent.putExtra("foodName", "김치찌개");
+
+            // 리뷰 화면으로 이동
+            startActivity(intent);
+        }
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
