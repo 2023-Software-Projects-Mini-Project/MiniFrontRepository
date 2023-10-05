6fe86360f5958a2f55a30d470562ad93f8ef2fa5
dilma01 <dilma01@naver.com>
2023-10-06 01:06:17 +0900

    친구추가 검색창에 친구 목록 뜨는 거

---
+++ app/src/main/java/kr/ac/duksung/minifrontapp/AddFriendsFragment.kt
@@ -0,0 +1,13 @@
+package kr.ac.duksung.minifrontapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddFriendsFragment : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_friends_fragment)
+    }
+}
+
+
