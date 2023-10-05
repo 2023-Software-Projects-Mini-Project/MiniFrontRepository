4935a9c42656da79b64ca0fed98e1c7fc3af6cb8
NYMoon0408 <wendy_angel@naver.com>
2023-10-06 06:12:05 +0900

    인텐트 객체로 정보 넘어가게

--- app/src/main/java/kr/ac/duksung/minifrontapp/BFoodActivity.kt
+++ app/src/main/java/kr/ac/duksung/minifrontapp/BFoodActivity.kt
@@ -4,18 +4,17 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.ImageView
 import kotlinx.android.synthetic.main.boonski_list.*
-import kotlinx.android.synthetic.main.kfood_list.*
+import kotlinx.android.synthetic.main.boonski_list.back_icon
 
 class BFoodActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.boonski_list)
 
-        val backIcon = findViewById<ImageView>(R.id.back_icon)
-
-        backIcon.setOnClickListener(object : View.OnClickListener {
+        back_icon.setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View?) {
                 finish() // 현재 액티비티 종료
             }
@@ -23,19 +22,19 @@ class BFoodActivity : AppCompatActivity() {
 
         goKimbab.setOnClickListener{
             val intent = Intent(this@BFoodActivity, MenuReviewActivity::class.java)
-            intent.putExtra("MenuName", "김밥")
+            intent.putExtra("menuName", "김밥")
             startActivity(intent)
         }
 
         goRamen.setOnClickListener{
             val intent = Intent(this@BFoodActivity, MenuReviewActivity::class.java)
-            intent.putExtra("MenuName", "라면")
+            intent.putExtra("menuName", "라면")
             startActivity(intent)
         }
 
         goTtokppokki.setOnClickListener{
             val intent = Intent(this@BFoodActivity, MenuReviewActivity::class.java)
-            intent.putExtra("MenuName", "떡볶이")
+            intent.putExtra("menuName", "떡볶이")
             startActivity(intent)
         }
     }
--- app/src/main/java/kr/ac/duksung/minifrontapp/KFoodActivity.kt
+++ app/src/main/java/kr/ac/duksung/minifrontapp/KFoodActivity.kt
@@ -7,38 +7,35 @@ import android.view.View
 import android.widget.ImageView
 import kotlinx.android.synthetic.main.kfood_list.*
 
-
 class KFoodActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.kfood_list)
 
+        back_icon.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                finish() // 현재 액티비티 종료
+            }
+        })
+
         goKimchi.setOnClickListener {
             val intent = Intent(this@KFoodActivity, MenuReviewActivity::class.java)
-            intent.putExtra("MenuName", "김치찌개")
+            intent.putExtra("menuName", "김치찌개")
             startActivity(intent)
         }
 
         goDoenjang.setOnClickListener{
             val intent = Intent(this@KFoodActivity, MenuReviewActivity::class.java)
-            intent.putExtra("MenuName", "된장찌개")
+            intent.putExtra("menuName", "된장찌개")
             startActivity(intent)
         }
 
-        goBoodea.setOnClickListener{
+        goBoodea.setOnClickListener {
             val intent = Intent(this@KFoodActivity, MenuReviewActivity::class.java)
-            intent.putExtra("MenuName", "부대찌개")
+            intent.putExtra("menuName", "부대찌개")
             startActivity(intent)
         }
-
-
-
-        val backIcon = findViewById<ImageView>(R.id.back_icon)
-
-        backIcon.setOnClickListener(object : View.OnClickListener {
-            override fun onClick(v: View?) {
-                finish() // 현재 액티비티 종료
-            }
-        })
     }
 }
